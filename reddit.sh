1b42f6488dd0e9b6bf82397795b11b0b13ca9644
Guillaume Huysmans <ghuysmans99@gmail.com>
2018-10-24 18:37:55 +0200

    --no-clobber, UA, after

--- reddit.sh
+++ reddit.sh
@@ -1,11 +1,15 @@
-#!/bin/sh
+#!/bin/sh -e
 [ -z "$1" ] && echo usage: $0 subreddit && exit 1
-#FIXME save the output, iterate using .data.after
-curl https://www.reddit.com/r/$1.json?limit=100 |
-jq -r '.data.children|
+[ -z "$2" ] || after="&after=$2"
+UA="linux:github.com/ghuysmans/im2yolo:0.1"
+curl --user-agent "$UA" "https://www.reddit.com/r/$1.json?limit=100$after" |
+jq -r '.data.after, (
+       .data.children|
        map(.data)|
        map(select((.is_video=false) and (.is_self=false)))|
        map(.url|select(test(".jpg$")))
-       []' |
-#FIXME avoid duplicates...
-(cd /tmp; mkdir $1; cd $1; xargs -P 20 wget)
+       [])' | (
+read next
+N=`cd /tmp; mkdir -p $1; cd $1; xargs wget --no-verbose --no-clobber; ls |wc -l`
+echo N=$N $next
+)
